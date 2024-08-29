4f3f0591e5f152f8333d1466e883a9ac2bff875a
codin-son <izzfreak69@gmail.com>
2024-08-29 12:21:54 +0800

    Return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -7,12 +7,12 @@ import (
 	"example.com/greetings"
 )
 
-func main(){
+func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
-	if err != nil{
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
